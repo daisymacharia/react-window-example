7aa669593940edd444bf86b34f293493b5ea3702
daisymacharia <daisy.macharia@andela.com>
2020-07-07 15:35:07 +0300

    Updates

---
+++ precache-manifest.c8a9e8167a0bf64e522d0ec0c1450fe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea8631f875ad706b852565cde06342d",
+    "url": "/react-window-example/index.html"
+  },
+  {
+    "revision": "e1165dde66b0676030bc",
+    "url": "/react-window-example/static/css/main.cfcd0503.chunk.css"
+  },
+  {
+    "revision": "85925920e2e49c7486ea",
+    "url": "/react-window-example/static/js/2.28c27b29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-window-example/static/js/2.28c27b29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1165dde66b0676030bc",
+    "url": "/react-window-example/static/js/main.7485cf83.chunk.js"
+  },
+  {
+    "revision": "9609cbdd2fa9dea696ff",
+    "url": "/react-window-example/static/js/runtime-main.0533d94e.js"
+  }
+]);
